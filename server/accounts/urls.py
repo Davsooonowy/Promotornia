8ee3bd41b1ba58bd1e11904a57768a605d140e14
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-05-20 14:53:11 +0200

    link url

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('dean/users/', views.DeanView.as_view(), name='dean_delete'),
     path('user/login/', views.LoginView.as_view(), name='login'),
-    path('user/login/refresh/', TokenRefreshView.as_view(), name='refresh')
+    path('user/login/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path("auth/view-password/", views.OneTimePasswordView.as_view(), name="view_password")
 ]
